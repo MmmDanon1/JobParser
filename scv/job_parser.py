e930566b3c0da43047d8a01e045bc7659aa689d5
Yura <Orl0ff.1ur@yandex.ru>
2023-08-07 22:42:31 +0300

    fix fun

--- scv/job_parser.py
+++ scv/job_parser.py
@@ -29,17 +29,8 @@ class HHCollectingVacancies(CollectingVacancies):
         response_json = response.json()
         list_vacancy = []
         for item in response_json['items']:
-            try:
-                name = item['employer']['name']
-                url = item['employer']['url']
-                requirements = item['experience']['name'], item['employment']['name']
-                pay = item['salary']['from']
-                list_vacancy.extend([{"name": name, "url": url, "requirements": requirements, "pay": pay}])
-            except:
-                ""
-        if list_vacancy == []:
-            return "по данному запросу ничего не найдено"
-        return list_vacancy
+            list_vacancy.extend(item)
+        return response_json
 
 class SuperJobCollectingVacancies(CollectingVacancies):
     """
@@ -58,20 +49,7 @@ class SuperJobCollectingVacancies(CollectingVacancies):
                      }
         response = requests.get(url=SuperJobCollectingVacancies.url_sj, headers=headers_hh, params=params_hh)
         response_json = response.json()
-        list_vacancy = []
-        for object in response_json["objects"]:
-            try:
-                name = object['client']['title']
-                url = object['client']['url']
-                requirements = object['candidat']
-                pay = object['payment_from']
-                list_vacancy.extend([{"name": name, "url": url, "requirements": requirements, "pay": pay}])
-            except:
-                ""
-        if list_vacancy == []:
-            return "по данному запросу ничего не найдено"
-        return list_vacancy[0:search_quantity]
-
+        return response_json
 
 class WorkingWithVacancies:
     """
@@ -83,11 +61,62 @@ class WorkingWithVacancies:
         self.pay = pay
         self.requirements = requirements
 
+    def __repr__(self):
+        return f'_{__class__.__name__}("{self.name}","{self.url}","{self.requirements}",{self.pay})'
+
     def __str__(self):
         """
         информация для пользователя
         """
         return f'Название фирмы: {self.name},\nCсылка на вакансию: {self.url},\nТребования: {self.requirements},\nОплата от {self.pay} рублей'
+
+    @classmethod
+    def sj_vacancy(cls, list_vacancys, search_quantity):
+        list_vacancy = []
+        for object in list_vacancys["objects"]:
+            try:
+                name = object['client']['title']
+                if name == None or "":
+                    name = "название фирмы не указано"
+                url = object['client']['url']
+                if url == None or "":
+                    url = "ссылка на вакансию не указана"
+                requirements = object['candidat']
+                if requirements == None or "":
+                   requirements = "требований нет"
+                pay = object['payment_from']
+                if pay == None:
+                    pay = "сумма оплаты после собеседования"
+                list_vacancy.append(cls(name, url, requirements, pay))
+            except:
+                ""
+        if list_vacancy == []:
+            return "по данному запросу ничего не найдено"
+        return list_vacancy[0:search_quantity]
+
+    @classmethod
+    def hh_vacancy(cls, list_vacancys):
+        list_vacancy = []
+        for item in list_vacancys['items']:
+            try:
+                name = item['employer']['name']
+                if name == None or "":
+                    name = "название фирмы не указано"
+                url = item['employer']['url']
+                if url == None or "":
+                    url = "ссылка на вакансию не указана"
+                requirements = f"{item['experience']['name']}, {item['employment']['name']}"
+                if requirements == None or "":
+                   requirements = "требований нет"
+                pay = item['salary']['from']
+                if pay == None:
+                    pay = "сумма оплаты после собеседования"
+                list_vacancy.append(cls(name, url, requirements, pay))
+            except:
+                ""
+        if list_vacancy == []:
+            return "по данному запросу ничего не найдено"
+        return list_vacancy
 
     def __add__(self, other):
         """
